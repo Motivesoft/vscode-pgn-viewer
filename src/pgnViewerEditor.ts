416a387031b29a2544fcb91faf72b799706b2f4e
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-11-10 13:23:08 +0100

    Initial complete version

--- src/pgnViewerEditor.ts
+++ src/pgnViewerEditor.ts
@@ -45,6 +45,12 @@ export class PgnViewerEditorProvider implements vscode.CustomTextEditorProvider 
   }
 
   private getHtmlForWebview(webview: vscode.Webview): string {
+    // OK, so make this work in both the development environment and when built using 'vcse package', then we do this:
+    // 1) npm i @mliebelt/pgn-viewer
+    // 2) copy the dist.js from that package to our 'dist' folder using the "copy-files" script in package.json
+    //    - the dist folder is already going be part of the resultant vsix when packaged
+    //    - the copy is automated to occur when installing, packaging and debugging 
+    // 3) reference the copied module using the code, below, which will now work correctly whatever the launch semantics
     vscode.window.showInformationMessage(vscode.Uri.joinPath(this.context.extensionUri, 'dist', './dist.js').toString());
     const scriptUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this.context.extensionUri, 'dist', 'dist.js')
@@ -58,6 +64,19 @@ export class PgnViewerEditorProvider implements vscode.CustomTextEditorProvider 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="${scriptUri}"></script>
         <title>Chess Board</title>
+        <style>
+          body {
+            background-color: #F4F4F4;
+            font-family: Arial, sans-serif;
+            padding: 10px;
+          }
+          pre {
+            padding: 10px;
+            border-radius: 5px;
+            white-space: pre-wrap;
+            word-wrap: break-word;
+          }
+        </style>
       </head>
       <body>
         <div id="board"></div>
@@ -67,7 +86,19 @@ export class PgnViewerEditorProvider implements vscode.CustomTextEditorProvider 
             const message = event.data;
             switch (message.type) {
               case 'update':
-                PGNV.pgnView('board', { pgn: message.content });
+                PGNV.pgnView('board', { 
+                  pgn: message.content,
+                  figurine: "merida",
+                  layout: "left",
+                  notation: "long",
+                  notationLayout: "list",
+                  showFen: true,
+                  showResult: true,
+                  width: "1000px",
+                  boardSize: "400px",
+                  movesWidth: "400px",
+                  orientation: "white"
+                   });
                 break;
             }
           });
