db74d03597039d1d3c4b0b2edfb42af769b7047d
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-11-10 01:26:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# PGN Viewer
+
+This extension provides a viewer for chess Portable Game Notation (pgn) files. The viewer shows the game details, moves and a board on which to play through the moves.
+
+## Features
+
+The extension provides a mechanism `File Open...` for files with the extension `.pgn` and displays the contents along with a chess board.  
+
+## Requirements
+
+Uses pgn-viewer
+
+## Extension Settings
+
+None
+
+## Known Issues
+
+Results are unpredictable if the provided PGN data is invalid.
+
+## Release Notes
+
+
+### 1.0.0
+
+- Initial release
---
+++ src/pgnViewerEditor.ts
@@ -0,0 +1,79 @@
+import * as vscode from 'vscode';
+
+export class PgnViewerEditorProvider implements vscode.CustomTextEditorProvider {
+  public static register(context: vscode.ExtensionContext): vscode.Disposable {
+    const provider = new PgnViewerEditorProvider(context);
+    const providerRegistration = vscode.window.registerCustomEditorProvider(
+        PgnViewerEditorProvider.viewType,
+      provider
+    );
+    return providerRegistration;
+  }
+
+  private static readonly viewType = 'vscode-pgn-viewer.readonlyView';
+
+  constructor(private readonly context: vscode.ExtensionContext) {}
+
+  public async resolveCustomTextEditor(
+    document: vscode.TextDocument,
+    webviewPanel: vscode.WebviewPanel,
+    _token: vscode.CancellationToken
+  ): Promise<void> {
+    webviewPanel.webview.options = {
+      enableScripts: true,
+    };
+    webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview);
+
+    const updateWebview = () => {
+      webviewPanel.webview.postMessage({
+        type: 'update',
+        content: document.getText(),
+      });
+    };
+
+    const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(e => {
+      if (e.document.uri.toString() === document.uri.toString()) {
+        updateWebview();
+      }
+    });
+
+    webviewPanel.onDidDispose(() => {
+      changeDocumentSubscription.dispose();
+    });
+
+    updateWebview();
+  }
+
+  private getHtmlForWebview(webview: vscode.Webview): string {
+    vscode.window.showInformationMessage(vscode.Uri.joinPath(this.context.extensionUri, 'dist', './dist.js').toString());
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this.context.extensionUri, 'dist', 'dist.js')
+    );
+
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="${scriptUri}"></script>
+        <title>Chess Board</title>
+      </head>
+      <body>
+        <div id="board"></div>
+        <script>
+          const vscode = acquireVsCodeApi();
+          window.addEventListener('message', event => {
+            const message = event.data;
+            switch (message.type) {
+              case 'update':
+                PGNV.pgnView('board', { pgn: message.content });
+                break;
+            }
+          });
+        </script>
+      </body>
+      </html>
+    `;
+  }
+}
